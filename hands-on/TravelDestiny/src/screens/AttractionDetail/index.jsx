3c3e73a8cb7d18ed1fefd06549fb81c06bdc774c
Lauv <angelalauvis@gmail.com>
2024-01-24 17:57:03 +0700

    travel-destiny: add navbar and thumbnails view

--- hands-on/TravelDestiny/src/screens/AttractionDetail/index.jsx
+++ hands-on/TravelDestiny/src/screens/AttractionDetail/index.jsx
@@ -1,10 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Text, SafeAreaView, ImageBackground } from "react-native";
+import {
+    View,
+    Pressable,
+    SafeAreaView,
+    ImageBackground,
+    Image,
+} from "react-native";
 import styles from "./styles";
 
 function AttractionDetail({ route }) {
     const { item: attraction } = route?.params || {};
+    console.log(attraction.images.length);
     const mainImage = attraction?.images?.at(0);
     const navigation = useNavigation();
 
@@ -18,10 +25,35 @@ function AttractionDetail({ route }) {
                 style={styles.viewImage}
                 source={{ uri: mainImage }}
                 resizeMode="cover"
-                imageStyle={styles.image}
-            />
-            <Text onPress={onBack}>Back</Text>
-            <Text>Attraction Detail - {attraction.name}</Text>
+                imageStyle={styles.image}>
+                <View style={styles.navBar}>
+                    <Pressable onPress={onBack} hitSlop={8}>
+                        <Image
+                            style={styles.icon}
+                            source={require("../../assets/icons/back-button.png")}
+                        />
+                    </Pressable>
+                    <Pressable hitSlop={8}>
+                        <Image
+                            style={styles.icon}
+                            source={require("../../assets/icons/share-button.png")}
+                        />
+                    </Pressable>
+                </View>
+                <View style={styles.thumbnailsContainer}>
+                    {attraction?.images?.length
+                        ? attraction?.images
+                              .slice(0, 5)
+                              .map(image => (
+                                  <Image
+                                      key={image}
+                                      source={{ uri: image }}
+                                      style={styles.thumbnail}
+                                  />
+                              ))
+                        : null}
+                </View>
+            </ImageBackground>
         </SafeAreaView>
     );
 }
--- hands-on/TravelDestiny/src/screens/AttractionDetail/styles.js
+++ hands-on/TravelDestiny/src/screens/AttractionDetail/styles.js
@@ -3,9 +3,6 @@ import { Dimensions, StyleSheet } from "react-native";
 const { height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-    image: {
-        borderRadius: 20,
-    },
     container: {
         marginHorizontal: 22,
         marginTop: 48,
@@ -13,7 +10,38 @@ const styles = StyleSheet.create({
     viewImage: {
         width: "100%",
         height: height / 2,
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    image: {
         borderRadius: 20,
+    },
+    navBar: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+    },
+    icon: {
+        width: 36,
+        height: 36,
+        margin: 16,
+    },
+    thumbnailsContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alighItems: "center",
+        borderRadius: 15,
+        backgroundColor: `rgba(256, 256, 256, 0.35)`,
+        margin: 16,
+        paddingHorizontal: 16,
+    },
+    thumbnail: {
+        width: 40,
+        height: 40,
+        margin: 4,
+        borderRadius: 10,
     },
 });
 
