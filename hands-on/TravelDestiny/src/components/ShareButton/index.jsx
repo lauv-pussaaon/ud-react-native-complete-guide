a164fdec8089c2413c28a084cee6cdfd381bafb2
Lauv <angelalauvis@gmail.com>
2024-01-26 11:36:44 +0700

    travel-destiny: setup react native share

--- hands-on/TravelDestiny/src/components/ShareButton/index.jsx
+++ hands-on/TravelDestiny/src/components/ShareButton/index.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "./styles";
 import { TouchableOpacity, Image } from "react-native";
+import Share from "react-native-share";
 
-function ShareButton({ onShare }) {
+function ShareButton({ message, url }) {
+    const options = {
+        message: message || "My travel destiny...",
+        title: "Travel Destiny",
+        url: url || "http://traveldestiny.com/",
+    };
+    function onShare() {
+        async function share(options) {
+            const shareResponse = await Share.open(options);
+
+            console.log(shareResponse);
+        }
+
+        share(options);
+    }
+
     return (
         <TouchableOpacity
             onPress={onShare}
