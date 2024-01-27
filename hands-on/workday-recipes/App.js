c28890af79b39aa54944cbf46fb5c8479ba96641
Lauv <angelalauvis@gmail.com>
2024-01-27 17:17:27 +0700

    workday-recipes: fix header collapses while transitioning from splash screen

--- hands-on/workday-recipes/App.js
+++ hands-on/workday-recipes/App.js
@@ -16,7 +16,9 @@ export default function App() {
                 initialRouteName="Splash"
                 screenOptions={{
                     headerShadowVisible: false,
-                    headerTransparent: true,
+                    headerStyle: {
+                        backgroundColor: "#fff",
+                    },
                 }}
             >
                 <Stack.Screen
