bfeb42b0b8d31e27aa58ad36b171148fb592b1dd
Lauv <pussaaon@gmail.com>
2024-02-14 18:53:23 +0700

    planify: created new component TitleLight

---
+++ hands-on/planify/src/components/TitleLight/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import GlobalStyles from "../../constants/GlobalStyles";
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 24,
+        letterSpacing: 0.5,
+        color: GlobalStyles.colors.titleLight,
+        fontWeight: GlobalStyles.weights.semiLight,
+    },
+});
+
+export default styles;
